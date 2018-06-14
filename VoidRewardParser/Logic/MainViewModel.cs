ac7d243c3ebeeaec51cda411e7df844d7ea80a68
greenitthe123 <greenitthe@gmail.com>
2018-06-13 21:51:44 -0500

    Updated .NET & other packages, fixed minor bug
    Fixed bug with awaiting void, now runs as a task

--- VoidRewardParser/Logic/MainViewModel.cs
+++ VoidRewardParser/Logic/MainViewModel.cs
@@ -137,7 +137,7 @@ namespace VoidRewardParser.Logic
                 {
                     //Auto-record the selected reward if we detect a prime on the mission complete screen
                     _lastMissionComplete = DateTime.MinValue;
-                    await PrimeItems.FirstOrDefault(p => p.Visible)?.AddCommand?.Execute();
+                    await Task.Run(() => PrimeItems.FirstOrDefault(p => p.Visible)?.AddCommand?.Execute());
                 }
 
                 if (text.Contains(LocalizationManager.SelectAReward) && PrimeItems.Count - hiddenPrimes.Count > 0)
